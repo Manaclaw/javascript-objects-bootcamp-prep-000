43b644c725250640ef491d76acb5f538e361c78a
Steve <stevenjameswatkin@gmail.com>
2018-05-01 01:54:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,7 @@ var playlist = { "artistName": "songTitle" }
 playlist['Slowdive'] = 'Alison'
 playlist['My Bloody Valentine'] = 'Sometimes'
 
-function updatePlaylist
+function updatePlaylist(playlist, "artistName", "songTitle") {
+return Object.assign({playlist}, playlist, { ["artistName"]: "songTitle" })
+}
 
