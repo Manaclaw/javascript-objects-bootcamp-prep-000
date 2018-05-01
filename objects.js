9d5df2780ec06b2921b706a436fbd45a23c7f435
Steve <stevenjameswatkin@gmail.com>
2018-05-01 01:56:36 +0000

    automatically backed up by learn

--- objects.js
+++
@@ -1,9 +0,0 @@
-var playlist = { "artistName": "songTitle" }
-
-playlist['Slowdive'] = 'Alison'
-playlist['My Bloody Valentine'] = 'Sometimes'
-
-function updatePlaylist('laylist, artistName, songtitle) {
-return Obect.assign({playlist}, playlist, { ["artistName"]: "songTitle" })
-}
-
