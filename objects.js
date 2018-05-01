99c7614860cd915c47a51867b250b04fdb8c13b4
Steve <stevenjameswatkin@gmail.com>
2018-05-01 01:57:42 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { "artistName": "songTitle" }
+
+playlist['Slowdive'] = 'Alison'
+playlist['My Bloody Valentine'] = 'Sometimes'
+
+function updatePlaylist('laylist, artistName, songtitle) {
+return Obct.assign({playlist}, playlist, { ["artistName"]: "songTitle" })
+}
+
